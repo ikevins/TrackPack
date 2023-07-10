b6c2e2093af083331d5fba9751a417e3521bb9ab
ikevins <56990608+ikevins@users.noreply.github.com>
2023-07-10 13:19:30 -0400

    Update cameratest.py

--- cameratest.py
+++ cameratest.py
@@ -3,9 +3,9 @@ import time
 camera = PiCamera()
 time.sleep(2)
 camera.resolution = (1280, 720)
-camera.vflip = True
+camera.vflip = False
 camera.contrast = 10
-file_name = "/home/pi/Pictures/video_" + str(time.time()) + ".h264"
+file_name = "/home/ikevins/Desktop/testvideo.h264"
 print("Start recording...")
 camera.start_recording(file_name)
 camera.wait_recording(5)

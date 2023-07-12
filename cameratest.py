89fec036997cdd953a4ff28132ebe742df1b7ca2
ikevins <56990608+ikevins@users.noreply.github.com>
2023-07-12 14:06:30 -0400

    Update cameratest.py

--- cameratest.py
+++ cameratest.py
@@ -8,3 +8,8 @@ camera.contrast = 10
 camera.start_recording(/home/ikevins/Desktop/testvideo.h264)
 #camera.wait_recording(5)
 camera.stop_recording()
+
+# Define the command we want to execute.
+command = "MP4Box -add testvideo.h264 convertedVideo.mp4"
+# Execute our command
+call([command], shell=True)

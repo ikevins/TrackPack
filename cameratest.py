8de7741d9a3c2fa05df335e81fdbe633b344d403
ikevins <56990608+ikevins@users.noreply.github.com>
2023-07-12 14:20:33 -0400

    Update cameratest.py

--- cameratest.py
+++ cameratest.py
@@ -1,5 +1,6 @@
 from picamera import PiCamera
 import time
+import os
 camera = PiCamera()
 time.sleep(2)
 camera.resolution = (1280, 720)
@@ -9,7 +10,4 @@ camera.start_recording(/home/ikevins/Desktop/testvideo.h264)
 #camera.wait_recording(5)
 camera.stop_recording()
 
-# Define the command we want to execute.
-command = "MP4Box -add testvideo.h264 convertedVideo.mp4"
-# Execute our command
-call([command], shell=True)
+os.system("MP4Box -add testvideo.h264 convertedVideo.mp4")

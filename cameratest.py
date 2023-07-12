b337aef28511e40074e74511451ee6aef99fd2ef
ikevins <56990608+ikevins@users.noreply.github.com>
2023-07-12 14:24:51 -0400

    Update cameratest.py

--- cameratest.py
+++ cameratest.py
@@ -6,7 +6,7 @@ time.sleep(2)
 camera.resolution = (1280, 720)
 camera.vflip = False
 camera.contrast = 10
-camera.start_recording(/home/ikevins/Desktop/testvideo.h264)
+camera.start_recording("/home/ikevins/Desktop/testvideo.h264")
 #camera.wait_recording(5)
 camera.stop_recording()
 

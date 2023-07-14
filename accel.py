0814c68ea828fd00417d1451d6b71e8953372cc1
ikevins <56990608+ikevins@users.noreply.github.com>
2023-07-14 15:31:18 -0400

    Update accel.py

--- accel.py
+++ accel.py
@@ -1,43 +1,22 @@
 import smbus
 import time
-from math import sqrt
 
 bus = smbus.SMBus(1)
 address = 0x6B
 
-# Kalman filter variables
-Q = 0.001  # Process noise covariance
-R = 0.1  # Measurement noise covariance
-x = 0  # Initial state estimate
-P = 1  # Initial state covariance
-
-def kalman_filter(z):
-    global x, P
-    # Prediction
-    x_pred = x
-    P_pred = P + Q
-
-    # Kalman gain
-    K = P_pred / (P_pred + R)
-
-    # Update
-    x = x_pred + K * (z - x_pred)
-    P = (1 - K) * P_pred
-
-    return x
-
 while True:
     # Read accelerometer data
+    accel_x = bus.read_word_data(address, 0x28)
+    accel_y = bus.read_word_data(address, 0x2A)
     accel_z = bus.read_word_data(address, 0x2C)
 
-    # Convert raw data to acceleration value
+    # Convert the raw data to acceleration values
+    accel_x = accel_x / 16384.0
+    accel_y = accel_y / 16384.0
     accel_z = accel_z / 16384.0
 
-    # Apply Kalman filter
-    filtered_accel_z = kalman_filter(accel_z)
-
     # Calculate the total acceleration
-    acceleration = sqrt(filtered_accel_z ** 2)
+    acceleration = (accel_x ** 2 + accel_y ** 2 + accel_z ** 2) ** 0.5
 
     # Convert acceleration to g-forces
     gravity = 9.8  # Acceleration due to gravity in m/s^2

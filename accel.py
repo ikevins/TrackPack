37b8044bb1dce31ab103fc124aeecba81a41d7e4
ikevins <56990608+ikevins@users.noreply.github.com>
2023-07-16 22:33:14 -0400

    Update accel.py

--- accel.py
+++ accel.py
@@ -1,32 +1,8 @@
 import smbus
 import time
-import math
 
 bus = smbus.SMBus(1)
 address = 0x6B
-
-# Calibration variables
-num_samples = 100  # Number of samples to collect for calibration
-offset_x = 0.0
-offset_y = 0.0
-offset_z = 0.0
-
-# Collect samples for calibration
-print("Calibrating...")
-for _ in range(num_samples):
-    accel_x = bus.read_word_data(address, 0x28)
-    accel_y = bus.read_word_data(address, 0x2A)
-    accel_z = bus.read_word_data(address, 0x2C)
-
-    offset_x += accel_x
-    offset_y += accel_y
-    offset_z += accel_z
-
-offset_x /= num_samples
-offset_y /= num_samples
-offset_z /= num_samples
-
-print("Calibration completed.")
 
 while True:
     # Read accelerometer data
@@ -34,26 +10,13 @@ while True:
     accel_y = bus.read_word_data(address, 0x2A)
     accel_z = bus.read_word_data(address, 0x2C)
 
-    # Apply calibration offsets
-    accel_x -= offset_x
-    accel_y -= offset_y
-    accel_z -= offset_z
-
     # Convert the raw data to acceleration values
-    accel_x = accel_x / 16384.0
-    accel_y = accel_y / 16384.0
-    accel_z = accel_z / 16384.0
-
-    # Compensate for orientation
-    roll = math.atan2(accel_y, accel_z)
-    pitch = math.atan2(-accel_x, math.sqrt(accel_y * accel_y + accel_z * accel_z))
-
-    accel_x_comp = accel_x * math.cos(pitch) + accel_y * math.sin(roll) * math.sin(pitch) - accel_z * math.cos(roll) * math.sin(pitch)
-    accel_y_comp = accel_y * math.cos(roll) + accel_z * math.sin(roll)
-    accel_z_comp = accel_x * math.sin(pitch) - accel_y * math.sin(roll) * math.cos(pitch) + accel_z * math.cos(roll) * math.cos(pitch)
+    accel_x = accel_x / 0.000597
+    accel_y = accel_y / 0.000597
+    accel_z = accel_z / 0.000597
 
     # Calculate the total acceleration
-    acceleration = (accel_x_comp ** 2 + accel_y_comp ** 2 + accel_z_comp ** 2) ** 0.5
+    acceleration = (accel_x ** 2 + accel_y ** 2 + accel_z ** 2) ** 0.5
 
     # Convert acceleration to g-forces
     gravity = 9.8  # Acceleration due to gravity in m/s^2

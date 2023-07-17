1f2fc3a77520eafbb2080e0f6db8a50532084223
ikevins <56990608+ikevins@users.noreply.github.com>
2023-07-16 21:54:47 -0400

    Update accel2.py

--- accel2.py
+++ accel2.py
@@ -1,51 +1,54 @@
 import smbus
-import time
-import math
-from LSM9DS0 import *
-import datetime
+
+# I2C address of the LSM6DSL
+DEVICE_ADDRESS = 0x6B
+
+# LSM6DSL registers
+WHO_AM_I_REG = 0x0F
+CTRL1_XL_REG = 0x10
+CTRL2_G_REG = 0x11
+OUTX_L_XL_REG = 0x28
+OUTY_L_XL_REG = 0x2A
+OUTZ_L_XL_REG = 0x2C
+OUTX_L_G_REG = 0x22
+OUTY_L_G_REG = 0x24
+OUTZ_L_G_REG = 0x26
+
+# Initialize the I2C bus
 bus = smbus.SMBus(1)
- 
- 
-def writeACC(register,value):
-        bus.write_byte_data(ACC_ADDRESS , register, value)
-        return -1
- 
- 
-def readACCx():
-        acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_X_L_A)
-        acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_X_H_A)
-    acc_combined = (acc_l | acc_h <<8)
-    return acc_combined  if acc_combined < 32768 else acc_combined - 65536
- 
-def readACCy():
-        acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_Y_L_A)
-        acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_Y_H_A)
-    acc_combined = (acc_l | acc_h <<8)
-    return acc_combined  if acc_combined < 32768 else acc_combined - 65536
- 
-def readACCz():
-        acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_Z_L_A)
-        acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_Z_H_A)
-    acc_combined = (acc_l | acc_h <<8)
-    return acc_combined  if acc_combined < 32768 else acc_combined - 65536
- 
- 
-     
-#initialise the accelerometer
-writeACC(CTRL_REG1_XM, 0b01100111) #z,y,x axis enabled, continuos update,  100Hz data rate
-writeACC(CTRL_REG2_XM, 0b00011000) #+/- 8G full scale
- 
-while True:
-     
-     
-    #Read the accelerometer,gyroscope and magnetometer values
-    ACCx = readACCx()
-    ACCy = readACCy()
-    ACCz = readACCz()
-    print("##### X = %f G  #####" % ((ACCx * 0.244)/1000)),
-    print(" Y =   %fG  #####" % ((ACCy * 0.244)/1000)),
-    print(" Z =  %fG  #####" % ((ACCz * 0.244)/1000))
-     
-     
-    #slow program down a bit, makes the output more readable
-    time.sleep(0.03)
+
+def read_byte(reg):
+    return bus.read_byte_data(DEVICE_ADDRESS, reg)
+
+def read_word(reg):
+    high_byte = bus.read_byte_data(DEVICE_ADDRESS, reg)
+    low_byte = bus.read_byte_data(DEVICE_ADDRESS, reg + 1)
+    value = (high_byte << 8) | low_byte
+    return value
+
+def read_acceleration():
+    x = read_word(OUTX_L_XL_REG)
+    y = read_word(OUTY_L_XL_REG)
+    z = read_word(OUTZ_L_XL_REG)
+    return (x, y, z)
+
+def read_gyroscope():
+    x = read_word(OUTX_L_G_REG)
+    y = read_word(OUTY_L_G_REG)
+    z = read_word(OUTZ_L_G_REG)
+    return (x, y, z)
+
+# Check the device ID
+who_am_i = read_byte(WHO_AM_I_REG)
+if who_am_i != 0x6A:
+    raise ValueError("LSM6DSL not found at address 0x6B.")
+
+# Configure the accelerometer and gyroscope
+bus.write_byte_data(DEVICE_ADDRESS, CTRL1_XL_REG, 0x60)  # Accelerometer: 208 Hz, ±2 g
+bus.write_byte_data(DEVICE_ADDRESS, CTRL2_G_REG, 0x60)   # Gyroscope: 208 Hz, ±2000 dps
+
+# Read and print sensor data
+acceleration = read_acceleration()
+gyroscope = read_gyroscope()
+print("Acceleration (mg): X = {}, Y = {}, Z = {}".format(*acceleration))
+print("Gyroscope (dps): X = {}, Y = {}, Z = {}".format(*gyroscope))

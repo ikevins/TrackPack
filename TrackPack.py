60e0e0ee05bc6fdcf431b28e1a3b77fe1744f35c
ikevins <56990608+ikevins@users.noreply.github.com>
2023-07-17 11:58:01 -0400

    Update TrackPack.py

--- TrackPack.py
+++ TrackPack.py
@@ -1,5 +1,7 @@
-#import obd
+import obd
 import random
+import smbus
+import math
 import time
 from tkinter import *
 from tkinter import font as tkFont
@@ -7,20 +9,137 @@ from datetime import datetime
 
 mainWindow = Tk()
 
-mainWindow.geometry("800x480")
-#mainWindow.attributes('-fullscreen',True)
+#mainWindow.geometry("800x480")
+mainWindow.attributes('-fullscreen',True)
 mainWindow.title("TrackPack")
 mainWindow.configure(bg = "#FFFFFF")
 
+# I2C address of the LSM6DSL accelerometer
+ACCELEROMETER_I2C_ADDRESS = 0x6B
+
+# I2C address of the LIS3MDL magnetometer
+MAGNETOMETER_I2C_ADDRESS = 0x1E
+
+# Register addresses of the accelerometer
+ACCELEROMETER_CTRL_REG1 = 0x10
+ACCELEROMETER_CTRL_REG8 = 0x17
+ACCELEROMETER_OUTX_L = 0x28
+ACCELEROMETER_OUTX_H = 0x29
+ACCELEROMETER_OUTY_L = 0x2A
+ACCELEROMETER_OUTY_H = 0x2B
+ACCELEROMETER_OUTZ_L = 0x2C
+ACCELEROMETER_OUTZ_H = 0x2D
+
+# Register addresses of the magnetometer
+MAGNETOMETER_CTRL_REG1 = 0x20
+MAGNETOMETER_CTRL_REG2 = 0x21
+MAGNETOMETER_CTRL_REG3 = 0x22
+MAGNETOMETER_CTRL_REG4 = 0x23
+MAGNETOMETER_OUTX_L = 0x28
+MAGNETOMETER_OUTX_H = 0x29
+MAGNETOMETER_OUTY_L = 0x2A
+MAGNETOMETER_OUTY_H = 0x2B
+MAGNETOMETER_OUTZ_L = 0x2C
+MAGNETOMETER_OUTZ_H = 0x2D
+
+# Initialize the I2C bus
+bus = smbus.SMBus(1) #1 for Raspberry Pi 2 and newer
+
+# Configure the accelerometer
+bus.write_byte_data(ACCELEROMETER_I2C_ADDRESS, ACCELEROMETER_CTRL_REG1, 0x50) # Set the accelerometer to 208 Hz, ±2g range
+bus.write_byte_data(ACCELEROMETER_I2C_ADDRESS, ACCELEROMETER_CTRL_REG8, 0xC0) # Enable high-pass filter to remove gravity offset
+
+# Configure the magnetometer
+bus.write_byte_data(MAGNETOMETER_I2C_ADDRESS, MAGNETOMETER_CTRL_REG2, 0x04) # Configure the Magnetometer
+bus.write_byte_data(MAGNETOMETER_I2C_ADDRESS, MAGNETOMETER_CTRL_REG1, 0x80) # Sensor Enabled
+bus.write_byte_data(MAGNETOMETER_I2C_ADDRESS, MAGNETOMETER_CTRL_REG1, 0x60) # Ultra High Performance Mode Selected for XY Axis
+bus.write_byte_data(MAGNETOMETER_I2C_ADDRESS, MAGNETOMETER_CTRL_REG4, 0x0C) # Ultra High Performance Mode Selected for Z Axis
+bus.write_byte_data(MAGNETOMETER_I2C_ADDRESS, MAGNETOMETER_CTRL_REG1, 0x1C) # Output Data Rate of 80 Hz Selected
+bus.write_byte_data(MAGNETOMETER_I2C_ADDRESS, MAGNETOMETER_CTRL_REG3, 0x00) # Continous Conversion Mode, 4 wire interface Selected
+bus.write_byte_data(MAGNETOMETER_I2C_ADDRESS, MAGNETOMETER_CTRL_REG2, 0x60) # 16 guass Full Scale
+
+# Constants for gravitational acceleration
+GRAVITY = 9.80665  # m/s^2
+G_FORCE = GRAVITY / 1000  # g
+
 def exit(e):
     mainWindow.destroy()
+
+def inertialMeasurementUnit():
+    # Read acceleration data from the accelerometer
+    accelerometer_x_l = bus.read_byte_data(ACCELEROMETER_I2C_ADDRESS, ACCELEROMETER_OUTX_L)
+    accelerometer_x_h = bus.read_byte_data(ACCELEROMETER_I2C_ADDRESS, ACCELEROMETER_OUTX_H)
+    accelerometer_y_l = bus.read_byte_data(ACCELEROMETER_I2C_ADDRESS, ACCELEROMETER_OUTY_L)
+    accelerometer_y_h = bus.read_byte_data(ACCELEROMETER_I2C_ADDRESS, ACCELEROMETER_OUTY_H)
+    accelerometer_z_l = bus.read_byte_data(ACCELEROMETER_I2C_ADDRESS, ACCELEROMETER_OUTZ_L)
+    accelerometer_z_h = bus.read_byte_data(ACCELEROMETER_I2C_ADDRESS, ACCELEROMETER_OUTZ_H)
+
+    # Read acceleration data from the accelerometer
+    magnetometer_x_l = bus.read_byte_data(MAGNETOMETER_I2C_ADDRESS, MAGNETOMETER_OUTX_L)
+    magnetometer_x_h = bus.read_byte_data(MAGNETOMETER_I2C_ADDRESS, MAGNETOMETER_OUTX_H)
+    magnetometer_y_l = bus.read_byte_data(MAGNETOMETER_I2C_ADDRESS, MAGNETOMETER_OUTY_L)
+    magnetometer_y_h = bus.read_byte_data(MAGNETOMETER_I2C_ADDRESS, MAGNETOMETER_OUTY_H)
+    magnetometer_z_l = bus.read_byte_data(MAGNETOMETER_I2C_ADDRESS, MAGNETOMETER_OUTZ_L)
+    magnetometer_z_h = bus.read_byte_data(MAGNETOMETER_I2C_ADDRESS, MAGNETOMETER_OUTZ_H)
+
+    # Convert the raw data to signed 16-bit values
+    accelerometer_x = (accelerometer_x_h << 8 | accelerometer_x_l)
+    if accelerometer_x > 32767:
+        accelerometer_x -= 65536
+
+    accelerometer_y = (accelerometer_y_h << 8 | accelerometer_y_l)
+    if accelerometer_y > 32767:
+        accelerometer_y -= 65536
+
+    accelerometer_z = (accelerometer_z_h << 8 | accelerometer_z_l)
+    if accelerometer_z > 32767:
+        accelerometer_z -= 65536
+
+    magnetometer_x = (magnetometer_x_h << 8 | magnetometer_x_l)
+    if magnetometer_x > 32767:
+        magnetometer_x -= 65536
+
+    magnetometer_y = (magnetometer_y_h << 8 | magnetometer_y_l)
+    if magnetometer_y > 32767:
+        magnetometer_y -= 65536
+
+    magnetometer_z = (magnetometer_z_h << 8 | magnetometer_z_l)
+    if magnetometer_z > 32767:
+        magnetometer_z -= 65536
+
+    # Calculate the g-force in each axis
+    g_x = accelerometer_x / 16384.0
+    g_y = accelerometer_y / 16384.0
+    g_z = accelerometer_z / 16384.0
+
+    # Calculate the total g-force
+    g_total = math.sqrt(g_x ** 2 + g_y ** 2 + g_z ** 2) - 1.0  # Subtract the 1g offset
+
+    # Display only positive g-force values
+    g_total = max(0, g_total)
+
+    # Calculate the direction
+    direction = ""
+    angle = math.degrees(math.atan2(magnetometer_x, magnetometer_y))
+    if angle < 0:
+        angle += 360
+    if 45 <= angle < 135:
+        direction = "East"
+    elif 135 <= angle < 225:
+        direction = "South"
+    elif 225 <= angle < 315:
+        direction = "West"
+    else:
+        direction = "North"
+
+    return direction, g_total
 
 def openBeginLoggingWindow():
     startTime = time.time()
 
     BeginLoggingWindow=Toplevel()
-    BeginLoggingWindow.geometry("800x480")
-    #BeginLoggingWindow.attributes('-fullscreen',True)
+    #BeginLoggingWindow.geometry("800x480")
+    BeginLoggingWindow.attributes('-fullscreen',True)
     BeginLoggingWindow.title("TrackPack Begin Parameter Logging")
     BeginLoggingWindow.configure(bg = "#FFFFFF")
 
@@ -208,23 +327,20 @@ def openBeginLoggingWindow():
         90.0,
         370.0,
         anchor="center",
-        text=str(speed),
         fill="#000000",
         font=("Inter", 36 * -1)
     )
-    BeginLoggingWindowCanvas.create_text(
+    accelerometerDisplay = BeginLoggingWindowCanvas.create_text(
         709.0,
         370.0,
         anchor="center",
-        text="1",
         fill="#000000",
         font=("Inter", 36 * -1)
     )
-    BeginLoggingWindowCanvas.create_text(
+    magnetometerDisplay = BeginLoggingWindowCanvas.create_text(
         406.0,
         389.0,
         anchor="center",
-        text="N",
         fill="#000000",
         font=("Inter", 40 * -1)
     )
@@ -309,6 +425,7 @@ def openBeginLoggingWindow():
         distancePerMilliSecond = (speed / 3600000) # Miles per ms
         distanceTravelled += distancePerMilliSecond
         speed = random.randint(0, 100)
+        sensorReadings = inertialMeasurementUnit()
         if (speed > maxSpeed):
             maxSpeed = speed
             print(maxSpeed)
@@ -371,6 +488,14 @@ def openBeginLoggingWindow():
             text=str(speed)
         )
         BeginLoggingWindowCanvas.itemconfig(
+            magnetometerDisplay,
+            text=sensorReadings[0]
+        )
+        BeginLoggingWindowCanvas.itemconfig(
+            accelerometerDisplay,
+            text=sensorReadings[1]
+        )
+        BeginLoggingWindowCanvas.itemconfig(
             loggingTime,
             text=str(elapsedTime) + "s"
         )
@@ -388,8 +513,8 @@ def saveLog(currentLog):
 
 def openNoLogsWindow():
     noLogsWindow=Toplevel()
-    noLogsWindow.geometry("800x480")
-    #noLogsWindow.attributes('-fullscreen',True)
+    #noLogsWindow.geometry("800x480")
+    noLogsWindow.attributes('-fullscreen',True)
     noLogsWindow.title("TrackPack Review Stored Data")
     noLogsWindow.configure(bg = "#FFFFFF")
 
@@ -429,8 +554,8 @@ def openNoLogsWindow():
 
 def openParameterLoggingWindow():
     ParameterLoggingWindow=Toplevel()
-    ParameterLoggingWindow.geometry("800x480")
-    #ParameterLoggingWindow.attributes('-fullscreen',True)
+    #ParameterLoggingWindow.geometry("800x480")
+    ParameterLoggingWindow.attributes('-fullscreen',True)
     ParameterLoggingWindow.title("TrackPack Parameter Logging")
     ParameterLoggingWindow.configure(bg = "#FFFFFF")
     global speed
@@ -547,8 +672,8 @@ def openStoredDataWindow():
         openNoLogsWindow()
     else:
         StoredDataWindow=Toplevel()
-        StoredDataWindow.geometry("800x480")
-        #StoredDataWindow.attributes('-fullscreen',True)
+        #StoredDataWindow.geometry("800x480")
+        StoredDataWindow.attributes('-fullscreen',True)
         StoredDataWindow.title("TrackPack Review Stored Data")
         StoredDataWindow.configure(bg = "#FFFFFF")
         StoredDataWindowCanvas = Canvas(
@@ -600,8 +725,8 @@ def openStoredDataWindow():
 
 def openStoredLogWindow(logs):
     StoredLogWindow=Toplevel()
-    StoredLogWindow.geometry("800x480")
-    #StoredLogWindow.attributes('-fullscreen',True)
+    #StoredLogWindow.geometry("800x480")
+    StoredLogWindow.attributes('-fullscreen',True)
     StoredLogWindow.title("TrackPack Review Stored Data")
     StoredLogWindow.configure(bg = "#FFFFFF")
 
@@ -858,8 +983,8 @@ def checkDTC():
 
 def openDTCWindow():
     DTCWindow=Toplevel()
-    DTCWindow.geometry("800x480")
-    #DTCWindow.attributes('-fullscreen',True)
+    #DTCWindow.geometry("800x480")
+    DTCWindow.attributes('-fullscreen',True)
     DTCWindow.title("TrackPack Diagnostic Information")
     DTCWindow.configure(bg = "#FFFFFF")
 
@@ -924,8 +1049,8 @@ def openDTCWindow():
 
 def openNoDTCWindow():
     noDTCWindow=Toplevel()
-    noDTCWindow.geometry("800x480")
-    #noDTCWindow.attributes('-fullscreen',True)
+    #noDTCWindow.geometry("800x480")
+    noDTCWindow.attributes('-fullscreen',True)
     noDTCWindow.title("TrackPack Diagnostic Information")
     noDTCWindow.configure(bg = "#FFFFFF")
 
@@ -965,8 +1090,8 @@ def openNoDTCWindow():
 
 def openVehicleMovingWindow():
     vehicleMovingWindow=Toplevel()
-    vehicleMovingWindow.geometry("800x480")
-    #vehicleMovingWindow.attributes('-fullscreen',True)
+    #vehicleMovingWindow.geometry("800x480")
+    vehicleMovingWindow.attributes('-fullscreen',True)
     vehicleMovingWindow.title("TrackPack Parameter Logging")
     vehicleMovingWindow.configure(bg = "#FFFFFF")
 
@@ -1006,8 +1131,8 @@ def openVehicleMovingWindow():
 
 def openMoreDataWindow():
     dataWindow=Toplevel()
-    dataWindow.geometry("800x480")
-    #dataWindow.attributes('-fullscreen',True)
+    #dataWindow.geometry("800x480")
+    dataWindow.attributes('-fullscreen',True)
     dataWindow.title("TrackPack OBD-II Data")
     dataWindow.configure(bg = "#FFFFFF")
 
@@ -1557,8 +1682,8 @@ def openMoreDataWindow():
 
 def openDataWindow():
     dataWindow=Toplevel()
-    dataWindow.geometry("800x480")
-    #dataWindow.attributes('-fullscreen',True)
+    #dataWindow.geometry("800x480")
+    dataWindow.attributes('-fullscreen',True)
     dataWindow.title("TrackPack OBD-II Data")
     dataWindow.configure(bg = "#FFFFFF")
 
@@ -1797,13 +1922,13 @@ def openDataWindow():
         dataWindow.after(1, update)
     update()
 
-#obd.logger.setLevel(obd.logging.DEBUG)
+obd.logger.setLevel(obd.logging.DEBUG)
 
-#connection = obd.Async("/dev/rfcomm0", protocol="6", baudrate="38400", fast=False, timeout = 30)
+connection = obd.Async("/dev/rfcomm0", protocol="6", baudrate="38400", fast=False, timeout = 30)
 
 #Continuously query until the amount of supported commands is greater than 100
-#while len(connection.supported_commands) < 100:
-    #connection = obd.Async("/dev/rfcomm0", protocol="6", baudrate="38400", fast=False, timeout = 30)
+while len(connection.supported_commands) < 100:
+    connection = obd.Async("/dev/rfcomm0", protocol="6", baudrate="38400", fast=False, timeout = 30)
 
 coolantTemperature = 0
 rpm = 0
@@ -1930,26 +2055,26 @@ def evapTracker (response):
         evapPressure = int(response.value.magnitude)
 
 # Start the OBD connection and add the callbacks
-#connection.watch(obd.commands.COOLANT_TEMP, callback=coolantTemperatureTracker)
-#connection.watch(obd.commands.RPM, callback=rpmTracker)
-#connection.watch(obd.commands.SPEED, callback=speedTracker)
-#connection.watch(obd.commands.THROTTLE_POS, callback=throttlePositionTracker)
-#connection.watch(obd.commands.FUEL_LEVEL, callback=fuelLevelTracker)
-#connection.watch(obd.commands.OIL_TEMP, callback=oilTemperatureTracker)
-#connection.watch(obd.commands.STATUS, callback=statusTracker)
-#connection.watch(obd.commands.GET_DTC, callback=dtcTracker)
-#connection.watch(obd.commands., callback=)
-#connection.watch(obd.commands.ENGINE_LOAD, callback=engineLoadTracker)
-#connection.watch(obd.commands.SHORT_FUEL_TRIM_1, callback=shortFuelTrim1Tracker)
-#connection.watch(obd.commands.LONG_FUEL_TRIM_1	, callback=longFuelTrim1Tracker)
-#connection.watch(obd.commands.SHORT_FUEL_TRIM_2, callback=shortFuelTrim2Tracker)
-#connection.watch(obd.commands.LONG_FUEL_TRIM_2	, callback=longFuelTrim2Tracker)
-#connection.watch(obd.commands.FUEL_PRESSURE, callback=fuelPressureTracker)
-#connection.watch(obd.commands.INTAKE_PRESSURE, callback=intakePressureTracker)
-#connection.watch(obd.commands.MAF, callback=mafTracker)
-#connection.watch(obd.commands.FUEL_TYPE, callback=fuelTypeList)
-#connection.watch(obd.commands.EVAP_VAPOR_PRESSURE, callback=evapTracker)
-#connection.start()
+connection.watch(obd.commands.COOLANT_TEMP, callback=coolantTemperatureTracker)
+connection.watch(obd.commands.RPM, callback=rpmTracker)
+connection.watch(obd.commands.SPEED, callback=speedTracker)
+connection.watch(obd.commands.THROTTLE_POS, callback=throttlePositionTracker)
+connection.watch(obd.commands.FUEL_LEVEL, callback=fuelLevelTracker)
+connection.watch(obd.commands.OIL_TEMP, callback=oilTemperatureTracker)
+connection.watch(obd.commands.STATUS, callback=statusTracker)
+connection.watch(obd.commands.GET_DTC, callback=dtcTracker)
+connection.watch(obd.commands., callback=)
+connection.watch(obd.commands.ENGINE_LOAD, callback=engineLoadTracker)
+connection.watch(obd.commands.SHORT_FUEL_TRIM_1, callback=shortFuelTrim1Tracker)
+connection.watch(obd.commands.LONG_FUEL_TRIM_1	, callback=longFuelTrim1Tracker)
+connection.watch(obd.commands.SHORT_FUEL_TRIM_2, callback=shortFuelTrim2Tracker)
+connection.watch(obd.commands.LONG_FUEL_TRIM_2	, callback=longFuelTrim2Tracker)
+connection.watch(obd.commands.FUEL_PRESSURE, callback=fuelPressureTracker)
+connection.watch(obd.commands.INTAKE_PRESSURE, callback=intakePressureTracker)
+connection.watch(obd.commands.MAF, callback=mafTracker)
+connection.watch(obd.commands.FUEL_TYPE, callback=fuelTypeList)
+connection.watch(obd.commands.EVAP_VAPOR_PRESSURE, callback=evapTracker)
+connection.start()
 
 mainWindowCanvas = Canvas(
     mainWindow,

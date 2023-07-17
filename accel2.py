c5abe0923f453e0092798475dc97853f00f6bb19
ikevins <56990608+ikevins@users.noreply.github.com>
2023-07-16 23:48:27 -0400

    Update accel2.py

--- accel2.py
+++ accel2.py
@@ -1,61 +1,60 @@
 import smbus
 import time
 
-# I2C address of the LSM6DSL
-DEVICE_ADDRESS = 0x6B
+# I2C address of the LSM6DSL accelerometer
+I2C_ADDRESS = 0x6B
 
-# LSM6DSL registers
-WHO_AM_I_REG = 0x0F
-CTRL1_XL_REG = 0x10
-CTRL2_G_REG = 0x11
-OUTX_L_XL_REG = 0x28
-OUTY_L_XL_REG = 0x2A
-OUTZ_L_XL_REG = 0x2C
-OUTX_L_G_REG = 0x22
-OUTY_L_G_REG = 0x24
-OUTZ_L_G_REG = 0x26
+# Register addresses of the accelerometer
+CTRL1_XL = 0x10
+OUTX_L_XL = 0x28
+OUTX_H_XL = 0x29
+OUTY_L_XL = 0x2A
+OUTY_H_XL = 0x2B
+OUTZ_L_XL = 0x2C
+OUTZ_H_XL = 0x2D
 
 # Initialize the I2C bus
-bus = smbus.SMBus(1)
+bus = smbus.SMBus(1)  # 1 for Raspberry Pi 2 and newer
 
-def read_byte(reg):
-    return bus.read_byte_data(DEVICE_ADDRESS, reg)
-
-def read_word(reg):
-    high_byte = bus.read_byte_data(DEVICE_ADDRESS, reg)
-    low_byte = bus.read_byte_data(DEVICE_ADDRESS, reg + 1)
-    value = (high_byte << 8) | low_byte
-    return value
-
-def read_acceleration():
-    x = read_word(OUTX_L_XL_REG)
-    y = read_word(OUTY_L_XL_REG)
-    z = read_word(OUTZ_L_XL_REG)
-    return (x, y, z)
-
-def read_gyroscope():
-    x = read_word(OUTX_L_G_REG)
-    y = read_word(OUTY_L_G_REG)
-    z = read_word(OUTZ_L_G_REG)
-    return (x, y, z)
-
-# Check the device ID
-who_am_i = read_byte(WHO_AM_I_REG)
-if who_am_i != 0x6A:
-    raise ValueError("LSM6DSL not found at address 0x6B.")
-
-# Configure the accelerometer and gyroscope
-bus.write_byte_data(DEVICE_ADDRESS, CTRL1_XL_REG, 0x60)  # Accelerometer: 208 Hz, ±2 g
-bus.write_byte_data(DEVICE_ADDRESS, CTRL2_G_REG, 0x60)   # Gyroscope: 208 Hz, ±2000 dps
+# Configure the accelerometer
+bus.write_byte_data(I2C_ADDRESS, CTRL1_XL, 0x50)  # Set the accelerometer to 208 Hz, ±2g range
 
 try:
     while True:
-        acceleration = read_acceleration()
-        gyroscope = read_gyroscope()
-        print("Acceleration (mg): X = {}, Y = {}, Z = {}".format(*acceleration))
-        print("Gyroscope (dps): X = {}, Y = {}, Z = {}".format(*gyroscope))
-        print("----")
-        time.sleep(0.5)  # Pause for 0.5 seconds before reading again
+        # Read acceleration data from the accelerometer
+        x_l = bus.read_byte_data(I2C_ADDRESS, OUTX_L_XL)
+        x_h = bus.read_byte_data(I2C_ADDRESS, OUTX_H_XL)
+        y_l = bus.read_byte_data(I2C_ADDRESS, OUTY_L_XL)
+        y_h = bus.read_byte_data(I2C_ADDRESS, OUTY_H_XL)
+        z_l = bus.read_byte_data(I2C_ADDRESS, OUTZ_L_XL)
+        z_h = bus.read_byte_data(I2C_ADDRESS, OUTZ_H_XL)
+
+        # Convert the raw data to signed 16-bit values
+        x = (x_h << 8 | x_l)
+        if x > 32767:
+            x -= 65536
+
+        y = (y_h << 8 | y_l)
+        if y > 32767:
+            y -= 65536
+
+        z = (z_h << 8 | z_l)
+        if z > 32767:
+            z -= 65536
+
+        # Calculate the g-force in each axis
+        g_x = x / 16384.0  # 16384 LSB/g for ±2g range
+        g_y = y / 16384.0
+        g_z = z / 16384.0
+
+        # Calculate the total g-force
+        g_total = (g_x ** 2 + g_y ** 2 + g_z ** 2) ** 0.5
+
+        # Print the g-force values
+        print(f"X: {g_x:.2f}g, Y: {g_y:.2f}g, Z: {g_z:.2f}g, Total: {g_total:.2f}g")
+
+        # Wait for a moment
+        time.sleep(0.1)
 
 except KeyboardInterrupt:
     pass

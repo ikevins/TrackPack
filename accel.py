f9e20ea8c478da4ea426aaaa71b4e3c236ce1fa9
ikevins <56990608+ikevins@users.noreply.github.com>
2023-07-16 23:35:03 -0400

    Update accel.py

--- accel.py
+++ accel.py
@@ -4,16 +4,21 @@ import time
 bus = smbus.SMBus(1)
 address = 0x6B
 
+# Calibration values (obtained through calibration process)
+x_offset = -4
+y_offset = -1
+z_offset = -0.2
+
 while True:
     # Read accelerometer data
     accel_x = bus.read_word_data(address, 0x28)
     accel_y = bus.read_word_data(address, 0x2A)
     accel_z = bus.read_word_data(address, 0x2C)
 
-    # Convert the raw data to acceleration values
-    accel_x = accel_x / 16384.0
-    accel_y = accel_y / 16384.0
-    accel_z = accel_z / 16384.0
+    # Apply calibration offsets
+    accel_x = (accel_x / 16384.0) - x_offset
+    accel_y = (accel_y / 16384.0) - y_offset
+    accel_z = (accel_z / 16384.0) - z_offset
 
     # Calculate the total acceleration
     acceleration = (accel_x ** 2 + accel_y ** 2 + accel_z ** 2) ** 0.5

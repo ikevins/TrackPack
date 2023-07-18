4f828800d04a80c65d4417748fbb1afa09c92a82
ikevins <56990608+ikevins@users.noreply.github.com>
2023-07-18 15:12:05 -0400

    Update cameratest.py

--- cameratest.py
+++ cameratest.py
@@ -5,6 +5,21 @@ camera = PiCamera()
 time.sleep(2)
 camera.resolution = (1280, 720)
 camera.rotation = 270
+camera.sharpness = 0
+camera.contrast = 0
+camera.brightness = 50
+camera.saturation = 0
+camera.ISO = 0
+camera.video_stabilization = False
+camera.exposure_compensation = 0
+camera.exposure_mode = 'auto'
+camera.meter_mode = 'average'
+camera.awb_mode = 'auto'
+camera.image_effect = 'none'
+camera.color_effects = None
+camera.rotation = 0
+camera.hflip = False
+camera.vflip = False
 camera.start_recording("/home/ikevins/Desktop/testvideo.h264")
 camera.wait_recording(5)
 camera.stop_recording()
